54d360629558affc268b0edc655634437b1099c3
saraNegm <sarra.negm@gmail.com>
2020-05-22 05:43:21 +0200

    Database Diagram and schema

---
+++ DataBase/What's Learn schema 1.sql
@@ -0,0 +1,211 @@
+
+create database whats
+
+create table [User]
+(
+	ID int primary key not null identity ,
+	[Name] nvarchar(250) not null ,
+	Email nvarchar(250) not null unique ,
+	[Password] nvarchar(250) not null ,
+	Age int not null ,
+	[Address] nvarchar(250) not null,
+	Gender bit not null,
+	Phone nvarchar(20) not null , 
+	IsActive bit not null , 
+	Education nvarchar(250) not null 
+)
+
+create table [Admin]
+(
+	ID int primary key not null identity ,
+	[Name] nvarchar(250) not null ,
+	Email nvarchar(250) not null unique ,
+	[Password] nvarchar(250) not null ,
+	Age int not null ,
+	[Address] nvarchar(250) not null,
+	Gender bit not null,
+	Phone nvarchar(20) not null , 
+	IsActive bit not null 
+)
+
+--create table UserSkill
+--(
+--	ID int primary key not null identity , 
+--	[Name] nvarchar(100),
+--	UserID int foreign key 
+
+--)
+
+Create table MainCategory (
+	ID int primary key not null identity , 
+	[Name] nvarchar(250) not null ,
+	Discription nvarchar(1000) not null 
+
+)
+
+Create table MainCategoryLink (
+	ID int primary key not null identity , 
+	Link nvarchar(250) not null ,
+	Discription nvarchar(1000) not null ,
+	MainCategoryID int FOREIGN KEY REFERENCES MainCategory(ID)
+)
+
+Create table MainCategoryDocument (
+	ID int primary key not null identity , 
+	[File] nvarchar(1000) not null ,
+	Discription nvarchar(1000) not null ,
+	MainCategoryID int FOREIGN KEY REFERENCES MainCategory(ID)
+)
+
+Create table MainCategoryVedio(
+	ID int primary key not null identity , 
+	Vedio nvarchar(1000) not null ,
+	Discription nvarchar(1000) not null ,
+	MainCategoryID int FOREIGN KEY REFERENCES MainCategory(ID)
+)
+
+---------------------------------------------------------------------------------
+
+Create table SubCategory (
+	ID int primary key not null identity , 
+	[Name] nvarchar(250) not null ,
+	Discription nvarchar(1000) not null ,
+	MainCategoryID int FOREIGN KEY REFERENCES MainCategory(ID)
+	   
+)
+
+Create table  SubCategoryLink (
+	ID int primary key not null identity , 
+	Link nvarchar(250) not null ,
+	Discription nvarchar(1000) not null ,
+	 SubCategoryID int FOREIGN KEY REFERENCES  SubCategory(ID)
+)
+
+Create table SubCategoryDocument (
+	ID int primary key not null identity , 
+	[File] nvarchar(1000) not null ,
+	Discription nvarchar(1000) not null ,
+	 SubCategoryID int FOREIGN KEY REFERENCES  SubCategory(ID)
+)
+
+Create table SubCategoryVedio(
+	ID int primary key not null identity , 
+	Vedio nvarchar(1000) not null ,
+	Discription nvarchar(1000) not null ,
+	 SubCategoryID int FOREIGN KEY REFERENCES  SubCategory(ID)
+)
+
+------------------------------------------------------------
+
+
+Create table Track (
+	ID int primary key not null identity , 
+	[Name] nvarchar(250) not null ,
+	Discription nvarchar(1000) not null ,
+	SubCategoryID int FOREIGN KEY REFERENCES SubCategory(ID)
+	   
+)
+
+Create table  TrackLink (
+	ID int primary key not null identity , 
+	Link nvarchar(250) not null ,
+	Discription nvarchar(1000) not null ,
+	 TrackID int FOREIGN KEY REFERENCES  Track(ID)
+)
+
+Create table TrackDocument (
+	ID int primary key not null identity , 
+	[File] nvarchar(1000) not null ,
+	Discription nvarchar(1000) not null ,
+	TrackID int FOREIGN KEY REFERENCES  Track(ID)
+)
+
+Create table TrackVedio(
+	ID int primary key not null identity , 
+	Vedio nvarchar(1000) not null ,
+	Discription nvarchar(1000) not null ,
+	 TrackID int FOREIGN KEY REFERENCES  Track(ID)
+)
+ --------------------------------------------------------------------
+ 
+
+Create table Course (
+	ID int primary key not null identity , 
+	[Name] nvarchar(250) not null ,
+	Discription nvarchar(1000) not null ,
+	TrackID int FOREIGN KEY REFERENCES Track(ID)
+	   
+)
+
+Create table  CourseLink (
+	ID int primary key not null identity , 
+	Link nvarchar(250) not null ,
+	Discription nvarchar(1000) not null ,
+	CourseID int FOREIGN KEY REFERENCES  Course(ID)
+)
+
+Create table CourseDocument (
+	ID int primary key not null identity , 
+	[File] nvarchar(1000) not null ,
+	Discription nvarchar(1000) not null ,
+	CourseID int FOREIGN KEY REFERENCES  Course(ID)
+)
+
+Create table CourseVedio(
+	ID int primary key not null identity , 
+	Vedio nvarchar(1000) not null ,
+	Discription nvarchar(1000) not null ,
+	CourseID int FOREIGN KEY REFERENCES  Course(ID)
+)
+
+-----------------------------------------------------------
+create table UserTrack 
+(
+	ID int primary key identity ,
+	UserID int not null FOREIGN KEY REFERENCES [User](ID), 
+	TrackID int not null FOREIGN KEY REFERENCES Track(ID),
+	[Date] datetime not null
+)
+
+create table FinishedCourse
+(
+	ID int primary key identity ,
+	CourseID int FOREIGN KEY REFERENCES  Course(ID),
+	UserTrackID int FOREIGN KEY REFERENCES  UserTrack(ID)
+)
+
+create table UserCertificate
+(
+	ID int primary key identity ,
+	Title nvarchar(250) not null, 
+	UserID int not null FOREIGN KEY REFERENCES  [User](ID)
+)
+
+create table UserSocialLink
+(
+	ID int primary key identity ,
+	Link nvarchar(500) not null, 
+	UserID int not null FOREIGN KEY REFERENCES  [User](ID)
+)
+
+create table Skill
+(
+	ID int primary key identity ,
+	Skill nvarchar(250) not null ,
+	
+)
+
+create table UserSkill
+(
+	ID int primary key identity ,
+	SkillID int not null FOREIGN KEY REFERENCES  Skill(ID),
+	UserID int not null FOREIGN KEY REFERENCES  [User](ID),
+	[Level] int not null,
+	unique (SkillID,UserID)
+)
+
+
+
+ALTER TABLE FinishedCourseADD CONSTRAINT K_FinishedCourse UNIQUE (CourseID,UserTrackID); 
+
+drop table UserSkill
